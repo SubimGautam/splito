3ab8971cc6bc72350bc6d0af2c7171a1a9c6a057
Subim Gautam <subimgautam@gmail.com>
2025-12-05 18:09:57 +0545

    Add Splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'signin_page.dart'; // Must point to your existing signin_page.dart
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const SignInScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/splito.png',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
