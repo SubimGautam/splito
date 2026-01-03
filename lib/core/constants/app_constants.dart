3545a99c85510e6d4f2307c3224f0aec8ee2588a
Subim Gautam <subimgautam@gmail.com>
2026-01-03 10:13:12 +0545

    app_constants update

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,21 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  //database name
+  static const String dbName = 'bill_splitter_app_db';
+
+  static const int batchTypeId = 0;
+  static const String batchTable = 'batch_table';
+
+  static const int itemTypeId = 1;
+  static const String itemTable = 'item_table';
+
+  static const int authTypeId = 2;
+  static const String authTable = 'auth_table';
+
+  static const int categoryTypeId = 0;
+  static const String categoryTable = 'category_table';
+
+  static const int commentTypeId = 0;
+  static const String commentTable = 'groups_table';
+}
